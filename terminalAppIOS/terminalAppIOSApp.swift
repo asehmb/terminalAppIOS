61f94afd5cd6b0e89d2657a03d9c4e208c8c2260
asehmb <130265153+asehmb@users.noreply.github.com>
2025-04-04 14:13:42 -0600

    created xcode proj

---
+++ terminalAppIOS/terminalAppIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  terminalAppIOSApp.swift
+//  terminalAppIOS
+//
+//  Created by Ajay Sehmbi on 2025-04-04.
+//
+
+import SwiftUI
+
+@main
+struct terminalAppIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ terminalAppIOSTests/terminalAppIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  terminalAppIOSTests.swift
+//  terminalAppIOSTests
+//
+//  Created by Ajay Sehmbi on 2025-04-04.
+//
+
+import Testing
+@testable import terminalAppIOS
+
+struct terminalAppIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
